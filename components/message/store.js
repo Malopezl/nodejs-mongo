cead2d32f5512173edeaf10dfda6b7f1f0a30f3c
Marvin Lopez <lopez.marvin9@gmail.com>
2022-11-01 20:06:29 -0600

    create connection to mongo & add methods to create and list messages

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,29 @@
-const list = [];
+const db = require('mongoose');
+const Model = require('./model');
+require('dotenv').config();
+
+db.Promise = global.Promise;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`;
+console.log(uri);
+
+db.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+console.log('[DB] Conectado con exito');
+
 
 function addMessage(message) {
-    list.push(message);
+    // list.push(message);
+    const myMessage = new Model(message);
+    myMessage.save();
 }
 
-function getMessages() {
-    return list;
+async function getMessages() {
+    // return list;
+    const messages = await Model.find().select('-__v');
+    return messages;
 }
 
 module.exports = {
