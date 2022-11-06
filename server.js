b7397bdffd55c6301fedbedc7412bc56ab1fbb2f
Marvin Lopez <lopez.marvin9@gmail.com>
2022-11-06 00:14:29 -0600

    connect API with websockets server

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+
 const bodyParser = require('body-parser');
-const router = require('./network/routes');
+const socket = require('./socket');
 const db = require('./db');
+const router = require('./network/routes');
 require('dotenv').config({ path: './.env' });
 
 /* Conexion a la base de datos */
 db(process.env.DB_CONNECT);
 
-const app = express();
-
 /* Codificacion */
 app.use(bodyParser.json());
+
+socket.connect(server);
 
 /* Se carga el servidor a las rutas para que inicie */
 router(app);
 
 app.use('/app', express.static('public'));
 
-app.listen(process.env.APP_PORT, () => {
+server.listen(process.env.APP_PORT, () => {
     console.log(`La aplicacion esta escuchando en http://localhost:${process.env.APP_PORT}`);
 });
